6c5e4038ffedc986a34e15d3c7fc46be8dc6cd9d
Michael Youmans <myoumans2013@gmail.com>
2026-06-22 17:45:55 -0400

    Add a method to delete interviews in Job applications

--- src/ApplicationCard.jsx
+++ src/ApplicationCard.jsx
@@ -2,6 +2,8 @@ import {useEffect, useState} from "react";
 
 function ApplicationCard({applications, handleDelete}) {
     const [interviews, setInterviews] = useState([]);
+    const applicationCount = applications.length;
+
 
     useEffect(() => {
         console.log("Interview state updated:", interviews);
@@ -16,9 +18,24 @@ function ApplicationCard({applications, handleDelete}) {
             .catch(error => console.log(error));
     }, []);
 
+    const handleDeleteInterview = (id) => {
+
+        fetch(`http://localhost:8080/api/interviews/deleteJobInterviewsByJobAppId/${id}`, {
+            method: "DELETE"
+        })
+            .then((response => {
+                if (!response.ok) {
+                    throw new Error("Interview was unable to be deleted.");
+                }
+                setInterviews(interviews.filter(interview =>
+                    interview.id !== id));
+            }))
+    }
+
 
     return (
         <ul className="application-card">
+            <span># of Applications - ({applicationCount})</span>
             <h2>Applications</h2>
 
             {applications.map((application) => {
@@ -28,95 +45,123 @@ function ApplicationCard({applications, handleDelete}) {
 
                 return (
                     <li className="application-individual-card" key={application.id}>
-                        <p>
-                            <strong className="text-before-input-box">Company Name: </strong>
-                            <span className="application-card-info">{application.companyName}</span>
-                        </p>
 
-                        <p>
-                            <strong className="text-before-input-box">Job Title: </strong>
-                            <span className="application-card-info">{application.jobTitle}</span>
-                        </p>
+                        <div style={{
+                            textAlign: "left",
+                            justifySelf: "start",
+                            alignSelf: "center",
+                        }}>App Id: {application.id}</div>
 
-                        <p>
-                            <strong className="text-before-input-box">Date Applied: </strong>
-                            <span className="application-card-info">{application.dateApplied}</span>
-                        </p>
+                        <div className={"application-text"}>
 
-                        <p>
-                            <strong className="text-before-input-box">Status: </strong>
-                            <span className="application-card-info">{application.status}</span>
-                        </p>
+                            <p>
+                                <strong className="text-before-input-box">Company Name: </strong>
+                                <span className="application-card-info">{application.companyName}</span>
+                            </p>
 
-                        <p>
-                            <strong className="text-before-input-box">Job Link: </strong>
-                            <span className="application-card-info">{application.jobLink}</span>
-                        </p>
+                            <p>
+                                <strong className="text-before-input-box">Job Title: </strong>
+                                <span className="application-card-info">{application.jobTitle}</span>
+                            </p>
 
-                        <p>
-                            <strong className="text-before-input-box">Notes: </strong>
-                            <span className="application-card-info">{application.notes}</span>
-                        </p>
+                            <p>
+                                <strong className="text-before-input-box">Date Applied: </strong>
+                                <span className="application-card-info">{application.dateApplied}</span>
+                            </p>
 
-                        <p>
-                            <strong className="text-before-input-box">Interview Count: </strong>
-                            <span className="application-card-info">{application.interviewCount}</span>
-                        </p>
-                        {matchingInterviews.length > 0 && (
-                            <details>
+                            <p>
+                                <strong className="text-before-input-box">Status: </strong>
+                                <span className="application-card-info">{application.status}</span>
+                            </p>
 
-                                <summary style={{
-                                    marginBottom: "10px",
-                                    fontStyle: "italic",
-                                    fontSize: "17px",
-                                    color: "honeydew"
-                                }}>Click to
-                                    view Interview(s)
-                                </summary>
+                            <p>
+                                <strong className="text-before-input-box">Job Link: </strong>
+                                <span className="application-card-info">{application.jobLink}</span>
+                            </p>
 
-                                <ul style={{borderRadius: "10px", listStyleType: "none", border: "1px solid white"}}>
-                                    {matchingInterviews.map((interview) => (
-                                        <li key={interview.id}>
-                                            <p>
-                                                <strong className="text-before-input-box">Interview Date: </strong>
-                                                <span className="application-card-info">{interview.interviewDate}</span>
-                                            </p>
+                            <p>
+                                <strong className="text-before-input-box">Notes: </strong>
+                                <span className="application-card-info">{application.notes}</span>
+                            </p>
 
-                                            <p>
-                                                <strong className="text-before-input-box">Interviewer Name: </strong>
-                                                <span
-                                                    className="application-card-info">{interview.interviewerName}</span>
-                                            </p>
+                            <p>
+                                <strong className="text-before-input-box">Interview Count: </strong>
+                                <span className="application-card-info">{application.interviewCount}</span>
+                            </p>
+                            {matchingInterviews.length > 0 && (
+                                <details>
 
-                                            <p>
-                                                <strong className="text-before-input-box">Interview Type: </strong>
-                                                <span
-                                                    className="application-card-info">{interview.interviewerType}</span>
-                                            </p>
+                                    <summary style={{
+                                        marginBottom: "10px",
+                                        fontStyle: "italic",
+                                        fontSize: "17px",
+                                        color: "honeydew"
+                                    }}>Click to
+                                        view Interview(s)
+                                    </summary>
 
-                                            <p>
-                                                <strong className="text-before-input-box">Notes: </strong>
-                                                <span className="application-card-info">{interview.notes}</span>
-                                            </p>
+                                    <ul style={{
+                                        borderRadius: "10px",
+                                        listStyleType: "none",
+                                        border: "1px solid white",
+                                        marginBottom: "10px",
+                                        textAlign: "center"
+                                    }}>
+                                        {matchingInterviews.map((interview) => (
+                                            <li key={interview.id}>
+                                                <p>
+                                                    <strong className="text-before-input-box">Interview Date: </strong>
+                                                    <span
+                                                        className="application-card-info">{interview.interviewDate}</span>
+                                                </p>
 
-                                            <p>
-                                                <strong className="text-before-input-box">Company Name: </strong>
-                                                <span className="application-card-info">{interview.companyName}</span>
-                                            </p>
+                                                <p>
+                                                    <strong className="text-before-input-box">Interviewer
+                                                        Name: </strong>
+                                                    <span
+                                                        className="application-card-info">{interview.interviewerName}</span>
+                                                </p>
 
-                                            <p>
-                                                <strong className="text-before-input-box">Job Title: </strong>
-                                                <span className="application-card-info">{interview.jobTitle}</span>
-                                            </p>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </details>
-                        )}
-                        <button className="button" onClick={() => handleDelete(application.id)}>
-                            Delete
-                        </button>
+                                                <p>
+                                                    <strong className="text-before-input-box">Interview Type: </strong>
+                                                    <span
+                                                        className="application-card-info">{interview.interviewerType}</span>
+                                                </p>
+
+                                                <p>
+                                                    <strong className="text-before-input-box">Notes: </strong>
+                                                    <span className="application-card-info">{interview.notes}</span>
+                                                </p>
+
+                                                <p>
+                                                    <strong className="text-before-input-box">Company Name: </strong>
+                                                    <span
+                                                        className="application-card-info">{interview.companyName}</span>
+                                                </p>
+
+                                                <p>
+                                                    <strong className="text-before-input-box">Job Title: </strong>
+                                                    <span className="application-card-info">{interview.jobTitle}</span>
+                                                </p>
+
+                                                <p>
+                                                    <button className="button"
+                                                            onClick={() => handleDeleteInterview(application.id)}>Delete
+                                                    </button>
+                                                </p>
+
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </details>
+
+                            )}
+                            <button className="button" onClick={() => handleDelete(application.id)}>
+                                Delete
+                            </button>
+                        </div>
                     </li>
+
                 );
             })}
         </ul>
