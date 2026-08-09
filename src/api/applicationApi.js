1ba124bcf25c0de0b002aebed97ed46450e6da1f
Michael Youmans <myoumans2013@gmail.com>
2026-08-09 18:01:31 -0400

    Add local testing environmental api variable

--- src/api/applicationApi.js
+++ src/api/applicationApi.js
@@ -1,5 +1,7 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const getApplications = async () => {
-    const response = await fetch("https://spring-boot-job-application-api.onrender.com/api/applications")
+    const response = await fetch(`${API_BASE_URL}/api/applications`)
 
     if (!response.ok) {
         throw new Error("Failed to fetch applications")
